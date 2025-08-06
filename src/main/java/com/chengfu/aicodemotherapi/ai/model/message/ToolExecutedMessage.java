640a0d0746f4425c8a432d99a27e65e971f5cbc4
chengfushi <303623518@qq.com>
2025-08-06 18:04:37 +0800

    feat(ai-project_generator): 添加 Vue 项目生成功能
    - 新增 Vue 项目生成的系统提示文件
    - 实现 Vue 项目生成的 AI服务接口和处理逻辑
    - 添加文件写入工具类，支持 AI 调用写入文件- 修改应用服务，支持 Vue项目生成和构建
    - 优化代码生成流的处理，支持工具调用和消息处理

---
+++ src/main/java/com/chengfu/aicodemotherapi/ai/model/message/ToolExecutedMessage.java
@@ -0,0 +1,34 @@
+package com.chengfu.aicodemotherapi.ai.model.message;
+
+/**
+ * @Author: Chengfu Shi
+ * @Description: 工具执行结果消息
+ **/
+
+import dev.langchain4j.service.tool.ToolExecution;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+public class ToolExecutedMessage extends StreamMessage {
+
+    private String id;
+
+    private String name;
+
+    private String arguments;
+
+    private String result;
+
+    public ToolExecutedMessage(ToolExecution toolExecution) {
+        super(StreamMessageTypeEnum.TOOL_EXECUTED.getValue());
+        this.id = toolExecution.request().id();
+        this.name = toolExecution.request().name();
+        this.arguments = toolExecution.request().arguments();
+        this.result = toolExecution.result();
+    }
+}
+
---
+++ src/main/java/com/chengfu/aicodemotherapi/ai/model/message/ToolRequestMessage.java
@@ -0,0 +1,31 @@
+package com.chengfu.aicodemotherapi.ai.model.message;
+
+/**
+ * @Author: Chengfu Shi
+ * @Description: 工具调用消息
+ **/
+
+import dev.langchain4j.agent.tool.ToolExecutionRequest;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+public class ToolRequestMessage extends StreamMessage {
+
+    private String id;
+
+    private String name;
+
+    private String arguments;
+
+    public ToolRequestMessage(ToolExecutionRequest toolExecutionRequest) {
+        super(StreamMessageTypeEnum.TOOL_REQUEST.getValue());
+        this.id = toolExecutionRequest.id();
+        this.name = toolExecutionRequest.name();
+        this.arguments = toolExecutionRequest.arguments();
+    }
+}
+
